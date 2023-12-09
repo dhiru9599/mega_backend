5db61056fbd8ae5075ed5583f0946851755f9909
Dhiru9599 <rajdhiraj1800@gmail.com>
2023-12-09 14:58:52 +0530

    disscussion of utils

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import dotenv from 'dotenv';
 import connectDB from './db/index.js';
 dotenv.config({ path: './env' });
+import { app } from './app.js';
 
 // import mongoose from 'mongoose';
 // import { db_name } from './constrnts.js';
 
 
-connectDB();
+connectDB()
+.then(()=>{
+  app.listen(process.env.PORT||5000,()=>{
+    console.log(`server is running on Prt ${process.env.PORT}`);
+  })
+})
+.catch((err),()=>{
+  console.log("mongodb connection failed!!!!",err);
+})
